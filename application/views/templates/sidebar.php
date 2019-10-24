db2a2a4ba0cde7e80c45137479fc18aefd033d9c
gadulz <gadul.46@gmail.com>
2019-10-24 16:05:45 +0700

    menu management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,50 +12,68 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+    <!-- Query Menu -->
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `user_menu`.`id`,`menu`
+                        FROM `user_menu` JOIN `user_access_menu`
+                        ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                        WHERE `user_access_menu`.`role_id` = $role_id
+                        ORDER BY `user_access_menu`.`menu_id` ASC ";
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+    $menu = $this->db->query($queryMenu)->result_array();
+    // var_dump($menu);
+    // die;
+    ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <!-- LOOPING MENU -->
+    <?php foreach ($menu as $mn) : ?>
+        <div class="sidebar-heading">
+            <?= $mn['menu']; ?>
+        </div>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
+        <!-- SUBMENU SESUAI MENU -->
+        <?php
+            $menuId = $mn['id'];
+            $querySubMenu = "SELECT *
+                                FROM `user_sub_menu` JOIN `user_menu`
+                                ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                                WHERE `user_sub_menu`.`menu_id` = $menuId
+                                AND `user_sub_menu`.`is_active` = 1 ";
 
-    <!-- Nav Item - Profile -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-user"></i>
-            <span>My Profile</span></a>
-    </li>
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+            ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+        <?php foreach ($subMenu as $smn) : ?>
+            <?php if ($title == $smn['title']) : ?>
+                <li class="nav-item active">
+                <?php else : ?>
+                <li class="nav-item">
+                <?php endif; ?>
+                <a class="nav-link" href="<?= base_url($smn['url']); ?>">
+                    <i class="<?= $smn['icon']; ?>"></i>
+                    <span><?= $smn['title']; ?></span></a>
+                </li>
+            <?php endforeach ?>
 
-    <!-- Nav Item - Logout -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
+            <hr class="sidebar-divider">
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+        <?php endforeach ?>
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+        <!-- Nav Item - Logout -->
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <span>Logout</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
 </ul>
 <!-- End of Sidebar -->
